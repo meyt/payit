688637ba9b01e52ec4c7d4f93abadcaa512cf120
meyt <pasd3000@gmail.com>
2017-11-12 02:17:56 +0330

    Add coveralls

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 import re
 import os.path
+import sys
 
 from setuptools import setup, find_packages
 
 package_name = 'pyment'
+py_version = sys.version_info[:2]
 
 # reading package's version (same way sqlalchemy does)
 with open(os.path.join(os.path.dirname(__file__), package_name, '__init__.py')) as v_file:
     package_version = re.compile(r".*__version__ = '(.*?)'", re.S).match(v_file.read()).group(1)
 
 dependencies = [
-  'zeep',
+    'zeep'
 ]
+
+if py_version < (3, 5):
+    dependencies.append('typing')
 
 setup(
     name=package_name,
