e8eb2d303235e11766abcef67f67725707d00f93
meyt <pasd3000@gmail.com>
2017-11-12 01:03:20 +0330

    Add `payir` tests

---
+++ pyment/tests/mockup/payir_gateway.py
@@ -0,0 +1,35 @@
+from urllib import error
+import json
+
+
+def get_side_effect(
+        returned_token='RETURNED_TOKEN',
+        returned_amount=100,
+        returned_status=1,
+        error_message=None,
+        error_code=None,
+        raise_url_error=False,
+        raise_http_error=False):
+
+    def urlopen(*args, **kwargs):
+        if raise_url_error:
+            raise error.URLError('FAKE ERROR')
+
+        if raise_http_error:
+            raise error.HTTPError(url='', code=raise_http_error, msg='FAKE ERROR', hdrs={}, fp=None)
+
+        class Response:
+            @staticmethod
+            def read():
+                return json.dumps(
+                    {
+                        'transId': returned_token,
+                        'status': returned_status,
+                        'amount': returned_amount,
+                        'errorMessage': error_message,
+                        'errorCode': error_code
+                    }
+                ).encode()
+
+        return Response()
+    return urlopen
