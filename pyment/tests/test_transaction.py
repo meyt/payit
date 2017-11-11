0c2db630d29ef49de50bc123e8eba8c23e721505
meyt <pasd3000@gmail.com>
2017-11-11 23:57:48 +0330

    Cover transaction

---
+++ pyment/tests/test_transaction.py
@@ -0,0 +1,21 @@
+import unittest
+
+from pyment import Transaction
+
+
+class TransactionTest(unittest.TestCase):
+
+    def test_transaction(self):
+        transaction = Transaction(
+            amount=1000,
+            order_id=1,
+            meta=dict(
+                card_number='xyz'
+            )
+        )
+        transaction_dict = transaction.to_dict()
+        self.assertEqual(transaction_dict['amount'], transaction.amount)
+
+
+if __name__ == '__main__':  # pragma: nocover
+    unittest.main()
