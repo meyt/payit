18c00f683ba61148c537d50c670445d92f933767
meyt <pasd3000@gmail.com>
2022-02-26 08:08:19 +0330

    Update gateways.payir

--- payit/gateways/payir.py
+++ payit/gateways/payir.py
@@ -1,6 +1,4 @@
-import json
-
-from urllib import request, parse, error
+from requests import request, RequestException
 
 from payit import (
     Gateway,
@@ -17,82 +15,121 @@ class PayIrGateway(Gateway):
 
     Home: https://pay.ir
     Documentation: https://pay.ir/docs/gateway/
+    Last Modification: 2022-02-26
     """
 
     __gateway_name__ = "payir"
     __gateway_unit__ = "IRR"
     __config_params__ = ["pin", "callback_url"]
 
+    _request_error_map = {
+        "-1": "ApiKeyNotFound",
+        "-2": "AmountNotFound",
+        "-3": "AmountNotInteger",
+        "-4": "AmountNotCorrect",
+        "-5": "RedirectUrlNotFound",
+        "-6": "PaymentGatewayNotFound",
+        "-7": "SellerNotActive",
+        "-8": "RedirectUrlsNotMatching",
+        "failed": "TransactionFailed",
+    }
+
+    _verify_error_map = {
+        "-1": "ApiKeyNotFound",
+        "-2": "TransIdNotFound",
+        "-3": "PaymentGatewayNotFound",
+        "-4": "SellerNotActive",
+        "-5": "TransactionFailed",
+    }
+
     def get_redirection(self, transaction) -> Redirection:
         return Redirection(
-            url="https://pay.ir/payment/gateway/%s" % transaction.id,
+            url="https://pay.ir/pg/%s" % transaction.id,
             method="get",
         )
 
     def request_transaction(self, transaction: Transaction) -> Transaction:
-        url = "https://pay.ir/payment/send"
-        data = {
-            "api": self.config["pin"],
+        url = "https://pay.ir/pg/send"
+        formdata = {
+            "api": "test" if self.testing else self.config["pin"],
             "amount": transaction.amount,
             "redirect": self.config["callback_url"],
         }
-        if self.testing:  # pragma: nocover
-            url = "https://pay.ir/payment/test/send"
-            data["api"] = "test"
+
+        if transaction.order_id:
+            formdata["factorNumber"] = transaction.order_id
+
+        optional_fields = (
+            "mobile",
+            "validCardNumber",
+            "description",
+        )
+        for field in optional_fields:
+            if field not in transaction.meta:
+                continue
+            formdata[field] = transaction.meta[field]
 
         try:
-            req = request.Request(url, data=parse.urlencode(data).encode())
-            resp = request.urlopen(req)
-            resp = json.loads(resp.read().decode())
-        except error.URLError:
-            raise GatewayNetworkError("Cannot connect to payline server.")
+            resp = request("post", url, data=formdata)
+        except RequestException:
+            raise GatewayNetworkError("Cannot connect to payir server")
 
-        if not (resp["status"] == 1 and resp["transId"]):
+        try:
+            body = resp.json()
+        except Exception:
+            body = None
+
+        if resp.status_code != 200:
+            if not body:
+                raise TransactionError(
+                    "Invalid transaction information (%s)" % resp.status_code
+                )
+
             raise TransactionError(
-                "%s, code: %s" % (resp["errorMessage"], resp["errorCode"])
+                "%s, code: %s" % (body["errorMessage"], body["errorCode"])
             )
 
-        transaction.id = resp["transId"]
+        transaction.id = body["token"]
         return transaction
 
     def validate_transaction(self, data: dict) -> Transaction:
         transaction = Transaction()
-        transaction.id = data["transId"]
+        transaction.id = data["token"]
         transaction.meta = data
-        if int(data["status"]) == 1:
-            transaction.validate_status = True
+        transaction.validate_status = int(data["status"]) == 1
         return transaction
 
     def verify_transaction(self, transaction: Transaction, data):
-        url = "https://pay.ir/payment/verify"
-        data = {
-            "api": self.config["pin"],
-            "transId": transaction.id,
+        url = "https://pay.ir/pg/verify"
+        formdata = {
+            "api": "test" if self.testing else self.config["pin"],
+            "token": transaction.id,
         }
-        if self.testing:  # pragma: nocover
-            url = "https://pay.ir/payment/test/verify"
-            data["api"] = "test"
+        try:
+            resp = request("post", url, data=formdata)
+        except RequestException:
+            raise GatewayNetworkError("Cannot connect to payir server")
 
         try:
-            req = request.Request(url, data=parse.urlencode(data).encode())
-            resp = request.urlopen(req)
-            resp = json.loads(resp.read().decode())
+            body = resp.json()
+        except Exception:
+            body = None
 
-        except error.HTTPError as e:
-            if e.code == 422:
-                raise TransactionError("Invalid transaction id")
-            raise TransactionError("Invalid transaction information")
+        if resp.status_code != 200:
+            if not body:
+                raise TransactionError(
+                    "Invalid transaction information (%s)" % resp.status_code
+                )
 
-        except error.URLError:
-            raise GatewayNetworkError("Cannot connect to payline server.")
-
-        if int(resp["status"]) != 1:
             raise TransactionError(
-                "%s, code: %s" % (resp["errorMessage"], resp["errorCode"])
+                "%s, code: %s" % (body["errorMessage"], body["errorCode"])
             )
 
-        if int(transaction.amount) != int(resp["amount"]):
+        if int(transaction.amount) != int(body["amount"]):
             raise TransactionError("Amount mismatch")
 
-        transaction.pan = resp["cardNumber"]
+        transaction.pan = body["cardNumber"]
+        transaction.amount = int(body["amount"])
+        transaction.order_id = body["factorNumber"]
+        transaction.meta = body
         return transaction
---
+++ tests/test_gateway_payir.py
@@ -0,0 +1,67 @@
+import mock
+import pytest
+
+from payit import Transaction, TransactionError, GatewayNetworkError
+from payit.gateways import PayIrGateway
+from tests.mockup.payir_gateway import get_side_effect
+
+
+config = {
+    "pin": "8523528",
+    "callback_url": "http://localhost/callback",
+}
+
+
+def domock(sideeffect):
+    return mock.patch("payit.gateways.payir.request", side_effect=sideeffect)
+
+
+def test_pay_ir():
+    gateway = PayIrGateway(config=config)
+    with domock(get_side_effect()):
+        transaction = gateway.request_transaction(
+            Transaction(amount=1000, order_id=1)
+        )
+        gateway.get_redirection(transaction)
+
+        valid_transaction = gateway.validate_transaction(
+            {"token": "RETURNED_TOKEN", "status": "1"}
+        )
+        assert valid_transaction.validate_status
+
+        invalid_transaction = gateway.validate_transaction(
+            {"token": "RETURNED_TOKEN", "status": "300"}
+        )
+        assert not invalid_transaction.validate_status
+
+    # success verification
+    with domock(get_side_effect(returned_status=1, returned_amount=1000)):
+        gateway.verify_transaction(transaction, dict())
+
+    # failed verification
+    with domock(get_side_effect(returned_status=300, returned_amount=400)):
+        with pytest.raises(TransactionError):
+            gateway.verify_transaction(transaction, dict())
+
+    # verification mismatch amount
+    with domock(get_side_effect(returned_status=1, returned_amount=600)):
+        with pytest.raises(TransactionError):
+            gateway.verify_transaction(transaction, dict())
+
+    # invalid transaction
+    with domock(get_side_effect(http_status_code=402)):
+        with pytest.raises(TransactionError):
+            gateway.verify_transaction(transaction, dict())
+
+    # invalid transaction id
+    with domock(get_side_effect(http_status_code=422)):
+        with pytest.raises(TransactionError):
+            gateway.verify_transaction(transaction, dict())
+
+    # verification network error
+    with domock(get_side_effect(raise_url_error=True)):
+        with pytest.raises(GatewayNetworkError):
+            gateway.verify_transaction(transaction, dict())
+
+        with pytest.raises(GatewayNetworkError):
+            gateway.request_transaction(Transaction(amount=1000, order_id=1))
